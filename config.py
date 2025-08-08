22aec18e8c2bca2f6940071e6010d589d8a801b7
Omkar Sonawale <Omkar.Sonawale@encodexindia.com>
2025-08-08 12:24:13 +0530

    D

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ DRIVE_SAVE_DIR = "/content/drive/MyDrive/m2d_transformer_decoder/saved_models_ss
 # Dataset paths (relative to cloned repo in Colab)
 DATASET_DIR = "./modi_dataset"
 NOISY_IMAGES_DIR = os.path.join(DATASET_DIR, "noisy_images")
-SYNTHETIC_IMAGES_DIR = os.path.join(DATASET_DIR, "synthetic_images")
+SYNTHETIC_IMAGES_DIR = os.path.join(DATASET_DIR, "clean_images")
 
 # Training constants
 BATCH_SIZE = 4
